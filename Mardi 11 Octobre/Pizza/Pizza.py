47568fd66e4d1f5e945800772f295697e1dfc10b
Wilk Michael <micka.wilk@gmail.com>
2022-10-11 16:19:27 +0200

    Push du Mardi 11 Octobre

--- Mardi 11 Octobre/Pizza/Pizza.py
+++ Mardi 11 Octobre/Pizza/Pizza.py
@@ -8,5 +8,15 @@ class Pizza:
     def display_pizza(self):
         print(self.name, self.ingredient)
 
+class Calzone(Pizza):
+    def __init__(self, name, ingredient, forme):
+        Pizza.__init__(self,name,ingredient)
+        self.forme = forme
+
+    def display_pizza(self):
+        print(self.name, self.ingredient, self.forme)
+
 pizza = Pizza("cocotte", "chocolatine")
 pizza.display_pizza()
+calzone = Calzone("cocotte", "chocolatine", "rond")
+calzone.display_pizza()
