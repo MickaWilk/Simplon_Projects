3e63783d4811be8be9c5bdde7a8b64b672f3c45d
Wilk Michael <micka.wilk@gmail.com>
2022-10-20 10:53:29 +0200

    modif nom dossier

--- Exos/Octobre 18 Mardi/get_github_api_names.py
+++ Exos/Octobre 18 Mardi/get_github_api_names.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import requests
-
+API_KEY = "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"
 
 df = pd.read_csv('akinator.csv', usecols=["github name"])
 ret = {}
@@ -9,8 +9,8 @@ for i in df.values.tolist():
     i = " ".join(i)
     print(i)
     try:
-        r = requests.get( f"https://github.com/{i}" )
-        ret[i] = bs(r.content).find("span", itemprop="name").getText().strip()
+        r = requests.get( f"https://api.github.com/user/", {'Authorization': f'Bearer {API_KEY}'} )
+        print(r)
     except:
         print("Requête non trouvée\n")
 [print(f"{i}:{j}") for i,j in ret.items()]
