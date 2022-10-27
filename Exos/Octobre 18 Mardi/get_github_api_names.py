a52a775dbe2073491f903dcb33f4de2becab9886
Wilk Michael <micka.wilk@gmail.com>
2022-10-27 11:38:55 +0200

    Exercices depuis 1 semaine

--- Exos/Octobre 18 Mardi/get_github_api_names.py
+++ Exos/Octobre 18 Mardi/get_github_api_names.py
@@ -6,11 +6,12 @@ API_KEY = "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"
 df = pd.read_csv('akinator.csv', usecols=["github name"])
 ret = {}
 for i in df.values.tolist():
-    print(i)
     i = "".join(i)
+    print(i)
     try:
         r = requests.get( f"https://api.github.com/users/{i}", auth=("MickaWilk",API_KEY) )
         print(r, r.json()['name'])
+        ret[i] = r.json()['name']
     except:
         print("Requête non trouvée\n")
-[print(f"{i}:{j}") for i,j in ret.items()]
+print(ret)
---
+++ Exos/Octobre 20 Jeudi/birthdayjam.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import requests
+
+
+df = pd.read_csv('akinator.csv', usecols=["github name"])
+ret = {}
+for i in df.values.tolist():
+    i = " ".join(i)
+    try:
+        r = requests.get( f"https://github.com/{i}" )
+        ret[i] = bs(r.content).find("span", itemprop="name").getText().strip()
+    except:
+        print("Requête non trouvée\n")
+[print(f"{i}:{j}") for i,j in ret.items()]
