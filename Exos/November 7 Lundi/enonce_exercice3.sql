1e8fbd9b37d9940ba30661aa982bc1a3d2ae61dd
Wilk Michael <micka.wilk@gmail.com>
2022-11-07 11:40:43 +0100

    Push music.db exercice

--- Exos/November 7 Lundi/enonce_exercice3.sql
+++ Exos/November 7 Lundi/enonce_exercice3.sql
@@ -2,35 +2,80 @@
 /* Exercise 1 */
 
 -- Query 1 : Display the CDs that came out in 1998. (line count: 35)
+SELECT * From Disc
+WHERE YEar = 1998
 
 -- Query 2 : Display the CDs published in 21st century. (line count: 142)
+SELECT * From Disc
+WHERE Year BETWEEN 2000 AND 2100
 
 -- Query 3 : Display the CDs that came out in the 90s. (line count: 291)
+SELECT * From Disc
+WHERE Year BETWEEN 1990 AND 1999
 
 -- Query 4 : Display the different names of states of CDs (without repretition). (line count: 4)
+SELECT DISTINCT State From Disc
+
 
 -- Query 5 : Find the titles of all damaged CDs. (line count: 170)
+SELECT Title From Disc
+WHERE State = 'Damaged'
 
 -- Query 6 : Find cheap used CDs (price below 11). Order the result by the price (ascending). (line count: 30)
+SELECT * From Disc
+WHERE State = 'Used' AND price < 11
+ORDER BY price ASC
 
 -- Query 7 : Find CDs that came out either in 1990 or 1995 and whose price is between 10 and 15. (line count: 26)
+SELECT * From Disc
+WHERE (Year = 1990 OR Year = 1995) AND price BETWEEN 10 AND 15
+
 
 -- Query 8 : Find the number of CDs that came out in the 80s and their price is between 12 and 14. (line count:
 42)
+SELECT * From Disc
+WHERE (Year BETWEEN 1980 AND 1989) AND (price BETWEEN 12 AND 14)
+
 
 -- Query 9 : Find the total of prices of destroyed CD. (the answer is 627.97) (line count: 1)
+SELECT SUM(price) From Disc
+WHERE State = 'Destroyed'
+
 
 -- Query 10 : Find CD by ’The Beatles’. (line count: 6)
+SELECT * From Disc
+JOIN Artist ON Artist.ID = Disc.Artist_ID
+WHERE Name LIKE 'The Beatles%'
+
 
 -- Query 11 : Find CDs by ’AC DC’ that came out in the 90s. (line count: 4)
+SELECT * From Disc
+JOIN Artist ON Artist.ID = Disc.Artist_ID
+WHERE Name LIKE 'AC DC' and Year BETWEEN 1990 AND 1999
+
 
 -- Query 12 : Find artists names who had a CD in 1991. (line count: 25)
+SELECT DISTINCT Name From Disc
+JOIN Artist ON Artist.ID = Disc.Artist_ID
+WHERE Year = 1991
+
 
 -- Query 13 : Find the sum of prices of all CD by Georges Brassens. (the answer is 200.73) (line count: 1)
+SELECT SUM(price) From Disc
+JOIN Artist ON Artist.ID = Disc.Artist_ID
+WHERE Name = 'Georges Brassens'
+
 
 -- Query 14 : Find titles of song of the Rock genre. (line count: 5504)
+SELECT Title
+FROM Song
+JOIN Genre as g ON g.id = Song.Genre_ID
+WHERE name = 'Rock'
+
 
 -- Query 15 : List titles of songs whose genre description uses the word ’afric’. (line count: 334)
+
+
 
 -- Query 16 : Find the number of folk songs whose lyrics are not covered by copyright. (the answer is 124) (line
 count: 1)
